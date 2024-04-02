2f236cdf2330f3b61d9b3d3fb2edd36c6051c518
Cecilia Wong <cecilia.wong.wy@gmail.com>
2024-04-02 21:44:05 +0800

    Configure logging settings with environment variable and .ini config file.

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask
 from logging.config import dictConfig
 
+import configparser
+from dotenv import load_dotenv
+import os
+
 
 def create_app():
     """
     The application factory.
     """
 
+    load_dotenv()
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    log_level = os.environ.get('LOG_LEVEL', config['Logging'].get('log_level'))
+    log_format = config['Logging'].get('format')
+
     dictConfig({
         'version': 1,
         'formatters': {'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+            'format': log_format,
         }},
         'handlers': {'wsgi': {
             'class': 'logging.StreamHandler',
@@ -18,10 +30,11 @@ def create_app():
             'formatter': 'default'
         }},
         'root': {
-            'level': 'INFO',
+            'level': log_level,
             'handlers': ['wsgi']
         }
     })
     
     app = Flask(__name__)
+    app.logger.debug(f'Configurations:-\nLog level: {log_level}\nLog format: {log_format}')
     return app
